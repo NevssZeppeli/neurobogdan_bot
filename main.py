28edc074cf8d823c6d48ee9857fd3b75e420df71
Nevss <mr.nevss@gmail.com>
2021-12-19 23:59:18 +0700

    Senders changed, private messages deleted

--- main.py
+++ main.py
@@ -7,25 +7,38 @@ session_api = vk_session.get_api()
 longpoll = VkBotLongPoll(vk_session, 209137754)
 
 
-# Здесь личные сообщения группы
-def sender(id, text):
-    vk_session.method('messages.send', {'user_id': id, 'message': text, 'random_id': 0})
-
-
 # Здесь для чатов
 def chat_sender(id, text):
-    vk_session.method('messages.send', {'chat_id': id, 'message': text, 'random_id': 0})
+    vk_session.method('messages.send',
+                      {'chat_id': id,
+                       'message': text,
+                       'random_id': 0
+                       })
+
+
+def photo_sender(id, url):
+    vk_session.method('messages.send',
+                      {'chat_id': id,
+                       'attachment': url,
+                       'random_id': 0
+                       })
 
 
 for event in longpoll.listen():
     if event.type == VkBotEventType.MESSAGE_NEW:
-        #чаты
+        # чаты
         if event.from_chat:
-            id = event.chat_id
+            chat_id = event.chat_id
             msg = event.object.message['text'].lower()
+            if msg[0:3] == '/nb':
+                # просто для себя в консоль вывожу
+                print(msg)
 
+        # личные сообщения
         if event.from_user:
-            id = event.user_id
+            user_id = event.user_id
             msg = event.object.message['text'].lower()
+            if msg[0:3] == '/nb':
+                print(msg)
 
 
